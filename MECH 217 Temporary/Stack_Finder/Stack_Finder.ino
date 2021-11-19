c1c00856b248e0dca9fa581a2b2cedd28c0d3d1f
Rick Sellens <sellensr@gmail.com>
2021-11-19 12:46:15 -0500

    Update Stack_Finder.ino

--- MECH 217 Temporary/Stack_Finder/Stack_Finder.ino
+++ MECH 217 Temporary/Stack_Finder/Stack_Finder.ino
@@ -11,6 +11,8 @@ unsigned long uLong2 = 42;
 unsigned long timeNow = 27;
 byte iByte2 = 42;
 
+int box[701] = {0};
+
 void setup() {
   // put your setup code here, to run once:
   unsigned long dt = micros();
@@ -31,6 +33,10 @@ void setup() {
   printAddr("uLong1", sizeof(uLong1),((unsigned) &uLong1) - BASE);
   printAddr("uLong2", sizeof(uLong2),((unsigned) &uLong2) - BASE);
   printAddr("timeLast", sizeof(timeLast),((unsigned) &timeLast) - BASE);
+  printAddr("box[0]", sizeof(box[0]),((unsigned) &(box[0])) - BASE);
+  printAddr("box[500]", sizeof(box[500]),((unsigned) &(box[500])) - BASE);
+  printAddr("box[700]", sizeof(box[700]),((unsigned) &(box[700])) - BASE);
+  Serial.println();
 }
 
 void printAddr(char *name, unsigned size, unsigned addr){
@@ -44,15 +50,18 @@ void printAddr(char *name, unsigned size, unsigned addr){
 void loop() {
   // put your main code here, to run repeatedly:
   int abc[] = {1,2,3,4,5,6,7,8,9,0};
-  factorial(millis()/1000, abc);
-  delay(3000);
+  factorial((millis()/5000)%20, abc);
+  delay(2000);
   Serial.print("\n");
 }
 
 unsigned long factorial(unsigned i, int abc[]){
   Serial.print("In factorial(i) for i = "); Serial.print(i);
+  delay(250);
   printAddr(", i ", sizeof(i),((unsigned) &i) - BASE);
-  printAddr("    abc ", sizeof(abc),((unsigned) &abc) - BASE);
+  delay(250);
+  printAddr("     abc ", sizeof(abc),((unsigned) &abc) - BASE);
+  delay(500);
   if(i < 2) return i;
   else return i * factorial(i-1,abc);
 }
