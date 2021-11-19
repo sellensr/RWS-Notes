d5ee0da26572072c761b05c1c6e2ff09d2035673
Rick Sellens <sellensr@gmail.com>
2021-11-19 10:42:50 -0500

    Update Blank_Starter.ino
    Added code to monitor and report loop timings

--- Code - Arduino/MECH 217 Assn Starters/Blank_Starter/Blank_Starter.ino
+++ Code - Arduino/MECH 217 Assn Starters/Blank_Starter/Blank_Starter.ino
@@ -8,6 +8,7 @@ void setup() {
 }
 
 void loop() {
+  uno.run();  // monitors your code for how fast it goes through loops
   // Update the global variables for the time in this loop
   timeLast = timeNow;      // the last time we went through the loop, microseconds
   timeNow = micros();      // the time we started this loop, microseconds
@@ -15,6 +16,8 @@ void loop() {
   
   // put your main code here, to run repeatedly:
 
-  
+  // Leave the rest of this code in place, but comment out when you don't need it
+  Serial.printf("Average dt = %8.1f us, this loop %8.1f us, maximum %8.1f\n", 
+                  uno.dtAvg(), uno.dt(), uno.dtMax());
   delay(20);  // delay only to slow things down so you can see
 }
